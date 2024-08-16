a852beced402ec552c40b51460e403f15f42ae53
hongzhe <hongzhe2022@163.com>
2024-08-16 10:38:19 +0800

    修改默认为debug

--- django/app/app/settings.py
+++ django/app/app/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q@*3!!#m3t51dur&bd!*1reh$*(jou^!xr74h6f8f8b_v8nq0q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
